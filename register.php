539dbe3a8cde29bb312596d13d25fcb469acae78
Ryan Potter <rjp24@uw.edu>
2017-04-28 11:46:16 -0700

    Fixed

--- register.php
+++ register.php
@@ -1,5 +1,4 @@
 <?php
-
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
@@ -9,21 +8,23 @@ error_reporting(E_ALL);
 	$username = 'rjp24';
 	$password = 'cructid';
 
+	$succ = "false";
 
 	try {
 
 		$db = new PDO($dsn, $username, $password);
-		$name = $_GET['user'];
+		$name = $_POST['user'];
+		$pass1 = $_POST['password1'];
+		$pass2 = $_POST['password2'];
 
-		$select_sql = "SELECT username, pwd FROM User WHERE username = '$name';";
-
-		$user_query = $db->query($select_sql);
-
-		$users = $user_query->fetchAll(PDO::FETCH_ASSOC);
-		if ($users) {
-			print json_encode($users);
+		if ($pass1 == $pass2) {
+			$insert_sql = "INSERT INTO User VALUES ('$name', '$pass');";
+			$user_insert = $db->query($insert_sql);
+			$db = null;
+			$succ = "true";
 		}
-		$db = null;
+
+
 	} catch (PDOException $e) {
 		$error_message = $e->getMessage();
 		echo 'There was an error connecting to the database.';
@@ -31,5 +32,5 @@ error_reporting(E_ALL);
 		exit();
 	}
 
-
+	print succ;
 ?>
